b6f18a344b47d2e650e10c46a2849193d4d41dce
Rishikesh <kesm3052021@gmail.com>
2025-07-21 23:57:50 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Timeline from "./Timeline";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Timeline />
+  </React.StrictMode>
+);
